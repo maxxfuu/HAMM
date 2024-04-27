8c17155df1bc1eef9ef6fecb5b92c903a3bea2da
Andre Lew <me@aelew.com>
2024-04-27 15:09:27 -0700

    add frontend starter (#2)
    * add frontend starter
    
    * update theme colors

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function HomePage() {
+  return (
+    <div>
+      <p>Hello world</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
