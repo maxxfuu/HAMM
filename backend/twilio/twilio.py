fb639e516248504db896d0ecbe47bed59095cb95
Max Fu <mfu27@ucmerced.edu>
2024-04-27 23:58:14 -0700

    working with twilio

---
+++ backend/twilio/twilio.py
@@ -0,0 +1,21 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+         body='This is the ship that made the Kessel Run in fourteen parsecs?',
+         from_='+15017122661',
+         media_url=['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg'],
+         to='+15558675310'
+     )
+
+print(message.sid) 
+
